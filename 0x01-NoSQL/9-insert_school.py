53354a0cf05c9c9f2795ab8b0d8ef45149bd11a9
Merannaa <meran.ahmed.tr@gmail.com>
2024-02-26 04:13:03 -0800

    sqlno

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -4,5 +4,5 @@
 
 def insert_school(mongo_collection, **kwargs):
         """Inserts a new document in a collection based on kwargs."""
-            res = mongo_collection.insert_one(kwargs)
-                return res.inserted_id
+        res = mongo_collection.insert_one(kwargs)
+        return res.inserted_id
