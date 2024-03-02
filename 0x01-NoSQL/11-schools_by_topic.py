737726e7afa98c192d197324ff60966e49137f70
Merannaa <meran.ahmed.tr@gmail.com>
2024-03-02 07:37:31 -0800

    nosql

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -3,10 +3,10 @@
 
 
 def schools_by_topic(mongo_collection, topic):
-        """Returns the list of school having a specific topic."""
-        topic_filter = {
-                'topics': {
-                    '$elemMatch': {'$eq': topic},
-                    },
-                }
-                return [doc for doc in mongo_collection.find(topic_filter)]
+    """Returns the list of school having a specific topic."""
+    topic_filter = {
+        'topics': {
+            '$elemMatch': {'$eq': topic},
+            },
+    }
+    return [doc for doc in mongo_collection.find(topic_filter)]
